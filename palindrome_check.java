3c3c9fd49b1387ddf63140f9823308bc6a7248cf
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-03-27 12:31:40 +0530

    palindrome_check

---
+++ palindrome_check.java
@@ -0,0 +1,16 @@
+public class palindrome_check {
+    public static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int rev = 0;
+        
+        while(num > 0) {
+            int digit = num % 10;
+            rev = rev * 10 + digit;
+            num /= 10;
+        }
+        
+        if(original == rev) System.out.println("Palindrome");
+        else System.out.println("Not Palindrome");
+    }
+}
