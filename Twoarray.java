e8dda18eed5fa927770434bbab64dfafe41ea0fe
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-04-15 21:01:40 +0530

    array 2d

--- Twoarray.java
+++ Twoarray.java
@@ -1,7 +1,6 @@
+import java.util.Scanner;
 
-
-
- public class Twoarray {
+public class Twoarray {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -12,15 +11,15 @@
         System.out.print("Enter the number of students: ");
         int n = sc.nextInt();
 
-        
         int[][] arr = new int[m][n];
 
-         for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("Enter marks for class " + i + " student " + j + ": ");
                 arr[i][j] = sc.nextInt();
             }
         }
+
         System.out.println("Markssss:");
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
