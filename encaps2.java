ab709e4a189343b42752a743ce34b52778b5a426
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-05-05 03:56:33 +0530

    encaps2

---
+++ encaps2.java
@@ -0,0 +1,26 @@
+class Student {
+    private int marks;
+
+    public void setMarks(int marks) {
+        if (marks >= 0 && marks <= 100) {
+            this.marks = marks;
+        } else {
+            System.out.println("Invalid marks!");
+        }
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+}
+
+public class encaps2 {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.setMarks(85);
+        System.out.println(s.getMarks());
+
+        s.setMarks(150);   // invalid
+    }
+}
