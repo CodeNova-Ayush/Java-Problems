41868552caf3352c790d70e023b327892137a307
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-05-29 23:51:38 +0530

    polymorphism 3

---
+++ polymorphism3.java
@@ -0,0 +1,27 @@
+class Animal {
+
+    static void type() {
+        System.out.println("This is an Animal");
+    }
+}
+
+class Dog extends Animal {
+
+    static void type() {
+        System.out.println("This is a Dog");
+    }
+}
+
+public class polymorphism3 {
+
+    public static void main(String[] args) {
+
+        Animal a = new Dog();
+
+        a.type();
+
+        Dog d = new Dog();
+
+        d.type();
+    }
+}
