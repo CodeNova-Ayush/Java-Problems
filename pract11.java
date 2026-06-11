5294b75e0b5a0a617338f996f6a46faeeaaf26b2
CodeNova-Ayush <ayushmishra991199@gmail.com>
2026-06-11 21:58:02 +0530

    encapsulation practice

---
+++ pract11.java
@@ -0,0 +1,36 @@
+class BankAccount {
+
+    private double balance;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+
+        if (amount <= balance) {
+            balance -= amount;
+        }
+
+        else {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+public class pract11 {
+
+    public static void main(String[] args) {
+
+        BankAccount b = new BankAccount();
+
+        b.deposit(5000);
+        b.withdraw(1000);
+
+        System.out.println("Balance: " + b.getBalance());
+    }
+}
